b7884620919f7605bccd867c1d99b4063b740435
Igor Voloshanenko <bob@example.com>
2021-10-09 20:38:51 +0300

    Typo fixes

--- build_prod.sh
+++ build_prod.sh
@@ -2,6 +2,6 @@
 source env.prod
 /bin/bash generate_prod.sh 
 docker build -t pcrqr .
-docker tag click2dial ${DOCKER_REGISTRY}/pcrqr:latest
+docker tag pcrqr ${DOCKER_REGISTRY}/pcrqr:latest
 docker push ${DOCKER_REGISTRY}/pcrqr:latest
 python deploy/deploy.py
